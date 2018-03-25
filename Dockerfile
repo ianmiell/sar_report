361cd9f50a656eb77ac715d3ca795b992fc26445
Ian Miell <ian.miell@gmail.com>
2018-03-25 20:21:35 +0100

    latest

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM debian
-RUN apt-get update -y && apt-get install -y gnuplot
+RUN apt-get update -y && apt-get install -y gnuplot vim sysstat locales
+RUN locale-gen en_US.UTF-8
 ADD . /
+WORKDIR /
+ENTRYPOINT /sar_report.sh
--- sar_report.sh
+++ sar_report.sh
@@ -5,19 +5,17 @@
 
 # ./sar_report && gnuplot sar_report.gnuplot && less output
 
-if [[ $DAY =  '' ]]
-then
-	DAY=$(date +%d)
-fi
-
 set -eu
-(
-	for i in -u -r -q -w -W -b
-	do
-		LC_TIME="en_US.UTF-8" sar -f /var/log/sysstat/sa${DAY} $i | tail -n +4 | head -n -1
-		echo "==========="
-		sleep 1
-	done
-) | awk -f sar_report.awk > input
+DAY=25
+rm -f sarout
+touch sarout
+for i in -u -r -q -w -W -b
+do
+	LC_TIME="en_US.UTF-8" sar -f /var/log/sysstat/sa${DAY} $i | tail -n +4 | head -n -1 >> sarout
+	echo "===========" >> sarout
+done
+cat sarout
+cat sarout | awk -f sar_report.awk > /tmp/input
+cat /tmp/input
 gnuplot sar_report.gnuplot
-cat output.ascii
+cat /tmp/output.ascii
