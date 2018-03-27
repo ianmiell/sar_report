d96b106eae4b5d828ff53ab41a0e83a558422c2d
Ian Miell <ian.miell@gmail.com>
2018-03-27 07:55:04 +0100

    report

--- sar_report.sh
+++ sar_report.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+
 # See this article for context:
 # See also: sar_report.awk, 
 # http://aionica.computerlink.ro/2011/02/visualize-sar-reports-with-awk-and-gnuplot/
 
-# ./sar_report && gnuplot sar_report.gnuplot && less output
-
 set -eu
-DAY=25
+DAY=$(date +%d)
 rm -f sarout
 touch sarout
 for i in -u -r -q -w -W -b
@@ -14,8 +13,8 @@ do
 	LC_TIME="en_US.UTF-8" sar -f /var/log/sysstat/sa${DAY} $i | tail -n +4 | head -n -1 >> sarout
 	echo "===========" >> sarout
 done
-cat sarout
 cat sarout | awk -f sar_report.awk > /tmp/input
 cat /tmp/input
 gnuplot sar_report.gnuplot
 cat /tmp/output.ascii
+rm -f sarout
